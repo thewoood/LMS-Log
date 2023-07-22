de69b9359777d817ec9ec0fe1e8645b764866706
hallaj <davwvod@gmail.com>
2023-07-22 06:03:24 +0330

    [DEL]: extra get/post funcs

--- python_files/ll_http_requests.py
+++ python_files/ll_http_requests.py
@@ -11,22 +11,3 @@ async def aio_post_request(session: aiohttp.ClientSession, url: str,
                            payload: dict) -> aiohttp.ClientResponse:
     async with session.post(url, data = payload) as response:
         return response
-        
-def get_request(url: str, cookies: dict=None) -> requests.Response:
-    with requests.Session() as session:
-        if cookies != None:
-            session.cookies.update(requests.utils.cookiejar_from_dict(cookies))
-        result = session.get(url=url, headers=dict(referer=url))
-        result.apparent_encoding
-    return result
-
-async def get_async_request(url: str, cookies: dict = None) -> requests.Response:
-    return await asyncio.to_thread(get_request, url)
-
-def post_request(url: str, _json: dict) -> requests.Response:
-    with requests.Session() as session:
-        response = session.post(url=url, json=_json)
-    return response
-
-async def post_async_request(url: str, _json: dict = None) -> requests.Response:
-    return await asyncio.to_thread(post_request, **{'url': url, '_json': _json})
